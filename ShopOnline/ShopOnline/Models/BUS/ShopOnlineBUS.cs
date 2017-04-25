23cc609213c510edeb935632db89495bd5561a20
huynhhien155 <countingstars155@gmail.com>
2017-04-25 11:36:48 +0700

    Phân trang

---
+++ ShopOnline/ShopOnline/Models/BUS/ShopOnlineBUS.cs
@@ -0,0 +1,22 @@
+﻿using ShopOnlineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopOnline.Models.BUS
+{
+    public class ShopOnlineBUS
+    {
+        public static  IEnumerable<SanPham> DanhSach()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<SanPham>("select * from SanPham ");
+        }
+        public static SanPham SanPham(int id)
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.SingleOrDefault<SanPham>("select * from SanPham where MaSanPham= @0 ",id);
+        }
+    }
+}
